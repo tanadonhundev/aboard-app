72369ab75ca0347f6e24c2c2be640a3befa85471
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-05-02 16:00:52 +0700

    refactor homePage

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center min-h-screen bg-base-grey-100 ">
+    <div className="flex justify-center min-h-screen bg-main-green-500 w-full">
       <div className="flex flex-col lg:flex-row-reverse w-full max-w-[1440px]">
         {/* ขวา - Banner (จะอยู่บนในมือถือ) */}
         <div className="w-full lg:w-1/2 bg-main-green-500 flex items-center justify-center">
-          <div className="w-full h-full bg-main-green-300 flex flex-col items-center justify-center text-white rounded-b-[36px] md:rounded-tl-[36px] md:rounded-br-none p-18">
+          <div className="w-full h-full bg-main-green-300 flex flex-col items-center justify-center text-white rounded-b-[36px] lg:rounded-tl-[36px] lg:rounded-br-none p-18">
             <Image
               src="/images/image.png"
               alt="Login Banner"
@@ -21,16 +21,18 @@ export default function SignInPage() {
         </div>
 
         {/* ซ้าย - Sign In (จะอยู่ล่างในมือถือ) */}
-        <div className="w-full lg:w-1/2 bg-main-green-500 flex items-center justify-center p-37">
-          <div className="flex flex-col">
-            <h1 className="text-[28px] text-white text-inter">Sign in</h1>
-            <div className="flex flex-col gap-4 mt-7 w-[384px]">
+        <div className="w-full lg:w-1/2 bg-main-green-500 flex items-center justify-center mt-30">
+          <div className="flex flex-col items-center justify-center w-full px-4">
+            <h1 className="text-[28px] text-white text-inter mb-4">Sign in</h1>
+            <div className="flex flex-col gap-4 w-full max-w-[384px] items-center">
               <Input
                 type="text"
                 placeholder="Username"
-                className="bg-white h-11"
+                className="bg-white h-11 text-lg w-[343px] sm:w-[300px] md:w-[350px] lg:w-[384px]"
               />
-              <Button className="bg-success h-10 text-lg">Sign In</Button>
+              <Button className="bg-success h-10 text-lg w-[343px] sm:w-[300px] md:w-[350px] lg:w-[384px]">
+                Sign In
+              </Button>
             </div>
           </div>
         </div>
